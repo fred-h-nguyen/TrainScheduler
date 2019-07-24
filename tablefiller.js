43161f94b151f3c64b9f33c746f20e7d6b43f074
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-07-23 19:03:23 -0500

    initialized firebase

---
+++ tablefiller.js
@@ -0,0 +1,14 @@
+ // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyClvqW6iB8T3MIvtg6aZsnoYB1n5T1_1Yg",
+    authDomain: "train-scheduler-4f8d3.firebaseapp.com",
+    databaseURL: "https://train-scheduler-4f8d3.firebaseio.com",
+    projectId: "train-scheduler-4f8d3",
+    storageBucket: "",
+    messagingSenderId: "470336295918",
+    appId: "1:470336295918:web:f95c8f5a96dfb31d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  
